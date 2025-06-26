6ebecbd713fb62461362fa485a6d236be0f04b7c
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-06-26 14:13:41 -0300

    Regra de negocio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,11 +11,30 @@ public class Dev {
 	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
 	private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 	
-	public void increverBootcamp(Bootcamp bootcamp) {}
+	public void increverBootcamp(Bootcamp bootcamp) {
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);		
+	}
 	
-	public void progredir() {}
+	public void progredir() {
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();	
+		if(conteudo.isPresent()) {
+			this.conteudoConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		}else {
+			System.err.println("Você não está matriculado em nenhum conteúdo");
+		}
+		
+	}
 	
-	public void calcularTotalXp() {}
+	public double calcularTotalXp() {
+		return this.conteudoConcluidos
+				.stream()
+				.mapToDouble(conteudo -> conteudo.calcularXp())
+				.sum();
+	}
+	
+	
 
 	public String getNome() {
 		return nome;
